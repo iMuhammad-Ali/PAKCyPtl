47fd826ddb835d4c34f1c021dd3eec764bbbd768
Muhammad Ali <105636293+iMuhammad-Ali@users.noreply.github.com>
2023-08-15 11:56:08 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB5pzwjigoza5a28nCRclUBNyakgp0wwCo",
-    authDomain: "pakcyptl.firebaseapp.com",
-    projectId: "pakcyptl",
-    storageBucket: "pakcyptl.appspot.com",
-    messagingSenderId: "1021359460779",
-    appId: "1:1021359460779:web:725dbcb369448b3e5942ee"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 initializeApp(firebaseConfig);
